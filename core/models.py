945a4091a3eedc5e44ddece990941986e89fa4d7
ddp-danilo <danilo.ddp1998@gmail.com>
2021-11-03 15:12:51 -0300

    Added table eventos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Evento(models.Model): # tabela
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data_do evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta: # metadados
+        db_table = 'evento' # nome da tabela no banco de dados
+
+    def __str__(self):
+        return self.titulo
